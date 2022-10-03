7af7f4476d474a1e279c18b22ed0a9a355cd457a
wwhtml <1129942397@qq.com>
2022-10-03 18:40:14 +0800

    vuecli、normalize.css、common.less、自动化引入variable.less/mixin.less、axios配置、vuex持久化配置

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <!-- 修改数据，测试是否持久化 -->
+    App {{ $store.state.user.profile.account }}
+    <button @click="$store.commit('user/setUser', { account: 'zhousg' })">
+      设置用户信息
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
