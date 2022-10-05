3dbdc4d20f2a1d89ebaa94e330d7801f6ffbb0aa
wwhtml <1129942397@qq.com>
2022-10-05 17:40:51 +0800

    首页主体-左侧分类：结构、弹层、处理品牌、骨架效果

--- src/main.js
+++ src/main.js
@@ -6,4 +6,8 @@ import store from "./store";
 import "normalize.css";
 import "@/assets/styles/common.less";
 
-createApp(App).use(store).use(router).mount("#app");
+import ui from "./components/library";
+//所谓的插件就是一种可以为vue添加全局功能的工具代码。一个插件可以是一个拥有install()方法的对象，也可以直接是一个安装函数本身（install函数）。
+
+// 插件的使用，在main.js使用app.use(插件)
+createApp(App).use(store).use(router).use(ui).mount("#app");
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="xtx-home-page">首页</div>
+  <div class="page-home">
+    <div class="home-entry">
+      <div class="container">
+        <!-- 左侧分类 -->
+        <HomeCategory></HomeCategory>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeCategory from "./components/home-category.vue";
 export default {
-  name: "xtx-home-page",
+  name: "page-home",
+  components: { HomeCategory },
 };
 </script>
 
