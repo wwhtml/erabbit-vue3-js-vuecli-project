1bdc06f36950acfb4fd87e65b3710aef2f0ee266
wwhtml <1129942397@qq.com>
2022-10-07 16:55:03 +0800

    首页主体-面板骨架效果（用在home-hot、home-new）

--- src/views/home/components/home-hot.vue
+++ src/views/home/components/home-hot.vue
@@ -1,24 +1,30 @@
 <template>
   <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
-    <ul ref="pannel" class="goods-list">
-      <li v-for="item in goods" :key="item.id">
-        <RouterLink to="/">
-          <img :src="item.picture" alt="" />
-          <p class="name">{{ item.title }}</p>
-          <p class="desc">{{ item.alt }}</p>
-        </RouterLink>
-      </li>
-    </ul>
+    <div style="position: relative; height: 426px">
+      <Transition name="fade">
+        <ul v-if="goods.length" class="goods-list">
+          <li v-for="item in goods" :key="item.id">
+            <RouterLink to="/">
+              <img :src="item.picture" alt="" />
+              <p class="name">{{ item.title }}</p>
+              <p class="desc">{{ item.alt }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+        <HomeSkeleton v-else />
+      </Transition>
+    </div>
   </HomePanel>
 </template>
 
 <script>
 import { ref } from "vue";
-import HomePanel from "./home-panel";
+import HomePanel from "./home-panel.vue";
+import HomeSkeleton from "./home-skeleton.vue";
 import { findHot } from "@/api/home";
 export default {
   name: "HomeNew",
-  components: { HomePanel },
+  components: { HomePanel, HomeSkeleton },
   setup() {
     const goods = ref([]);
     findHot().then((data) => {
--- src/views/home/components/home-new.vue
+++ src/views/home/components/home-new.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="home-new">
     <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
-      <template #right><XtxMore path="/" /></template>
-      <!-- 面板内容 -->
-      <ul class="goods-list">
-        <li v-for="item in goods" :key="item.id">
-          <RouterLink :to="`/product/${item.id}`">
-            <img :src="item.picture" alt="" />
-            <p class="name ellipsis">{{ item.name }}</p>
-            <p class="price">&yen;{{ item.price }}</p>
-          </RouterLink>
-        </li>
-      </ul>
+      <template v-slot:right><XtxMore /></template>
+      <div style="position: relative; height: 406px">
+        <Transition name="fade">
+          <ul v-if="goods.length" class="goods-list">
+            <li v-for="item in goods" :key="item.id">
+              <RouterLink to="/">
+                <img :src="item.picture" alt="" />
+                <p class="name">{{ item.name }}</p>
+                <p class="price">&yen;{{ item.price }}</p>
+              </RouterLink>
+            </li>
+          </ul>
+          <HomeSkeleton bg="#f0f9f4" v-else />
+        </Transition>
+      </div>
     </HomePanel>
   </div>
 </template>
 <script>
 import { ref } from "vue";
-import HomePanel from "./home-panel";
+import HomePanel from "./home-panel.vue";
+import HomeSkeleton from "./home-skeleton.vue";
 import { findNew } from "@/api/home";
 export default {
   name: "HomeNew",
-  components: { HomePanel },
+  components: { HomePanel, HomeSkeleton },
   setup() {
     const goods = ref([]);
     findNew().then((data) => {
